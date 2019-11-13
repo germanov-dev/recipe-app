68b223e28175501336fd41c77daa1bc876154d86
Dimitar Germanov <germanov.dev@gmail.com>
2019-11-13 14:32:39 +0200

    update package

---
+++ docs/precache-manifest.3f97052353cbe0a809dbef74ceaf4a86.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9646c5f282ac0b76b30e9c0ffc4eb217",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5f881433e503ca47331e",
+    "url": "./static/css/main.9849a6c9.chunk.css"
+  },
+  {
+    "revision": "973f3f307a1cd4110d1e",
+    "url": "./static/js/2.e3bde35c.chunk.js"
+  },
+  {
+    "revision": "5f881433e503ca47331e",
+    "url": "./static/js/main.3869ae02.chunk.js"
+  },
+  {
+    "revision": "2c395b6290ee7632ab3b",
+    "url": "./static/js/runtime-main.e499312c.js"
+  }
+]);
