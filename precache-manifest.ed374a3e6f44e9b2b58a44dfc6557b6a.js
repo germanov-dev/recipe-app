c2afb6691d5adacf80e216135c12628e01d3ebcb
Dimitar Germanov <germanov.dev@gmail.com>
2019-11-13 10:14:40 +0200

    add files

---
+++ precache-manifest.ed374a3e6f44e9b2b58a44dfc6557b6a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7839052b231417e9c0ad8ebcf7ced0d6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "be909ea8264498694a96",
+    "url": "/static/css/main.5ce9ce09.chunk.css"
+  },
+  {
+    "revision": "973f3f307a1cd4110d1e",
+    "url": "/static/js/2.e3bde35c.chunk.js"
+  },
+  {
+    "revision": "be909ea8264498694a96",
+    "url": "/static/js/main.3927d431.chunk.js"
+  },
+  {
+    "revision": "39d6e9881bb1f379b3c5",
+    "url": "/static/js/runtime-main.e272ac27.js"
+  }
+]);
